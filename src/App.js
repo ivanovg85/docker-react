078264bf1245c2202fef22b783ce9eb73253d350
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2020-10-31 23:06:57 +0100

    changed message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CORONA TEST :)
+          Go to sleep zzz
         </p>
         <a
           className="App-link"
